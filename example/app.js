d00aa40aced6fd6b33b4a25af311318eb9d6f013
Chris Jones <chris@jonezy.org>
2013-07-10 10:21:45 -0400

    example site fun
    Signed-off-by: Chris Jones <chris@jonezy.org>

--- example/app.js
+++ example/app.js
@@ -62,7 +62,7 @@ if ('development' == app.get('env')) {
   app.locals.isDebug = true;
 }
 
-app.get('/', routes.index);
+app.get('/:loc?', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- example/routes/index.js
+++ example/routes/index.js
@@ -1,19 +1,38 @@
 
 var nconf = require('nconf');
 var Reckon = require('../../lib/reckon');
+var request = require('request');
 
 nconf.argv().env().file({ file: 'settings.json'});
 
 exports.index = function(req, res){
   var reckoner = new Reckon({apiKey:nconf.get('apiKey'), units: 'ca'});
-
+  // these are some default lat and lon (liberty village);
   var lat = 43.6532;
   var lon = -79.3832;
+  if(req.params.loc) {
+    var locs = req.params.loc.split(',');
+    lat = locs[0];
+    lon = locs[1];
+  }
 
+  var geo_url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=<lat>,<lon>&sensor=true";
   reckoner.get({lat:lat,lon:lon}, function(data) {
-    res.render('index', { 
-      results:data,
-      fullResults:JSON.stringify(data, undefined, 2) 
+    geo_url = geo_url.replace('<lat>', lat);
+    geo_url = geo_url.replace('<lon>', lon);
+
+    request(geo_url, function(error, response, body) {
+      var address_data = JSON.parse(body);
+      var hood = address_data.results[0].address_components[2].short_name;
+      var city = address_data.results[0].address_components[4].short_name;
+      var country = address_data.results[0].address_components[7].short_name;
+
+      console.log("%s, %s, %s", hood, city, country);
+      res.render('index', {
+        location: hood + ', ' + city + ', ' + country,
+        results:data,
+        fullResults:JSON.stringify(data, undefined, 2) 
+      });
     });
   });
 };
