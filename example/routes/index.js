405c5c90194017ff196df0b4ea1abde4e3f14598
Chris Jones <chris@jonezy.org>
2013-07-10 07:19:13 -0400

    Implemented optional units qs parameter (close #1)
    Signed-off-by: Chris Jones <chris@jonezy.org>

--- example/routes/index.js
+++ example/routes/index.js
@@ -5,13 +5,15 @@ var Reckon = require('../../lib/reckon');
 nconf.argv().env().file({ file: 'settings.json'});
 
 exports.index = function(req, res){
-  var reckoner = new Reckon({apiKey:nconf.get('apiKey')});
+  var reckoner = new Reckon({apiKey:nconf.get('apiKey'), units: 'ca'});
 
   var lat = 43.6532;
   var lon = -79.3832;
 
   reckoner.get({lat:lat,lon:lon}, function(data) {
-    console.log();
-    res.render('index', { results:data, fullResults:JSON.stringify(data, undefined, 2) });
+    res.render('index', { 
+      results:data,
+      fullResults:JSON.stringify(data, undefined, 2) 
+    });
   });
 };
--- test/index.js
+++ test/index.js
@@ -6,6 +6,11 @@ var validOpts = {
   'apiKey':'1234'
 };
 
+var optionalOpts = {
+  'apiKey': '1234',
+  'units': 'ca'
+};
+
 var invalidOpts = {
   'zonk': '1234'
 };
@@ -20,6 +25,18 @@ describe('reckon constructor with valid arguments', function() {
   it('should have an apiKey that matches the apiKey in the arguments', function() {
     var reckon = new Reckon(validOpts);
     assert(reckon.apiKey, validOpts.apiKey, 'expected reckon.apiKey to equal validOpts.apiKey');
+  });
+
+  describe('with optional unit argument', function() {
+  it('should have an apiKey that matches the apiKey in the arguments', function() {
+    var reckon = new Reckon(optionalOpts);
+    assert(reckon.apiKey, optionalOpts.apiKey, 'expected reckon.apiKey to equal validOpts.apiKey');
+  });
+
+  it('should have an units that matches the units in the arguments', function() {
+    var reckon = new Reckon(optionalOpts);
+    assert(reckon.units, optionalOpts.units, 'expected reckon.units to equal optionalOpts.units');
+  });
   });
 });
 
